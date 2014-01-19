e15672347f0b32678c3b6ed3ac5178490bfb63c0
davidjahn <david@Pathos.(none)>
2014-01-19 02:29:08 -0500

    landing page

--- app/views/index.php
+++ app/views/index.php
@@ -1,10 +1,45 @@
 <!DOCTYPE html>
 <html>
-<body>
+	<head>
+	<title>Physics GRE Stats</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Google Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+	</head>
 
-<h1>My First Bun Heading</h1>
+	<body>
+	<div class="wrap">
+		<div class="content row" style="background: #222222;">
+			<div class="main col-lg-12">
+				<div class="page-header"><h1 class="text-center">COMING SOON</h1></div>
+			</div>
+		</div>
+		<div class="content row">
+			<div class="page-center"> <h2 class="text-center"> Physics GRE <em>Stats</em>.com </h2></div>
+		</div>
+		<div class="centered-container">
+		<div class="centered">
+			<p>Physics GRE Stats is a set of user-generated information about what types of GPA and GRE/PGRE scores were admitted to which Masters and PhD programs in physics, along with information about various physics grad programs. We should be functional on March 1st!</p>
+		</div>
+		</div>
+	</div>
 
-<p>My first paragraph.</p>
-
-</body>
+	
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://code.jquery.com/jquery.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
+	</body>
 </html>
+
